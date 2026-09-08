547aba0f52ea2cdb410c66e3150f3248268ce624
valerylot <lotvalery@gmail.com>
2026-09-08 14:32:55 -0700

    First Commit For Our First Lecture

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿//with the double forward slash, I am able to write a comment
+
+
+//WriteLine looks at our console and prints whatever is in the parentheses
+//WriteLines are considered Outputs
+Console.WriteLine("Hello, Chris & Valery!");
+
+//Console.Clear clears the console
+Console.Clear();
+
+//Syntax is the grammar of the programming language
+
+//Syntax errors / bugs are when we mess up, the syntax the error will appear in the console
+
+//"" is our string value aka just text
+Console.WriteLine("I want to eat some chipotle");
+
+//5 is an integer (number) without the ""
+Console.WriteLine(5);
+
+//Console.Write does not have a line break after, so any input after Console.Write will be Concatenated (joined) on to it
+Console.Write("Let's test this out");
